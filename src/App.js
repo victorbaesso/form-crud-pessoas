2dd1f81b255d3c9b42fa9bbf32a9c96a18324275
victorbae <victorbaesso00@gmail.com>
2021-03-09 18:01:55 -0300

    Formulario de cadastro de pessoas em etapas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Formulario from './components/Formulario/Formulario';
-import {Typography, Box, Container} from '@material-ui/core';
+import {Typography, Box, Container, Link} from '@material-ui/core';
 
 function App() {
   return (
@@ -9,6 +9,12 @@ function App() {
         <Typography variant="h5" component="h2">Cadastro de Pessoas</Typography>
           <Formulario onSubmit={onSubmit} />
       </Box> 
+      <Box align="center" mt={10}>
+        made by&nbsp;
+        <Link href="https://github.com/victorbae" color="primary">
+           J. Victor Baesso
+        </Link>
+      </Box>
     </Container>
   );
 }
